78b8baa10205d7fe36cfb6c307efa94aa919e235
MiguelViapiana <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-18 10:26:34 -0300

    Exercicio vesão 11

---
+++ Exercicio11.java
@@ -0,0 +1,8 @@
+public class Exercicio11 {
+    public static void executar(){
+        System.out.println("--- Lista de nome sexo das pessoas ---");
+        NomeSex nomeSex = new NomeSex();
+        nomeSex.inserirNomeSex();
+        nomeSex.imprimirNomeSexo();
+    }
+}
